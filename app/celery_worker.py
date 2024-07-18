e031b9d3de5edbec9f596c34988412dd7c2ed4b0
Himadri Talukder <htalukder@ibm.com>
2024-07-18 11:10:01 -0400

    added progress in celery task

--- app/celery_worker.py
+++ app/celery_worker.py
@@ -11,7 +11,9 @@ celery.conf.broker_url = os.environ.get("CELERY_BROKER_URL")
 celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND")
 
 
-@celery.task(name="create_task")
-def create_task(a):
-    time.sleep(a)
+@celery.task(bind=True, name="create_task")
+def create_task(self,a):
+    for i in range(10):
+        self.update_state(state='PROGRESS', meta={'current': i, 'total': 10})
+        time.sleep(a)
     return f"Slept for {a} seconds"
